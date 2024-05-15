790fff278ac841f08c6f1073b6e9473df8ff61fc
Max <bloodmailvol1@gmail.com>
2024-05-15 16:54:33 +0300

    main

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using Geometry;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isWorking = true;
+
+            while (isWorking)
+            {
+                ShowMenu();
+                string userInput = Console.ReadLine();
+
+                switch (userInput)
+                {
+                    case "1":
+                        CalculateCircleArea();
+                        break;
+                    case "2":
+                        CalculateTriangleArea();
+                        break;
+                    case "3":
+                        isWorking = false;
+                        break;
+                    default:
+                        ShowError();
+                        break;
+                }
+
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+
+        static void ShowMenu()
+        {
+            Console.WriteLine("Меню: ");
+            Console.WriteLine("1.Рассчитать площадь круга по радиусу.");
+            Console.WriteLine("2.Рассчитать площадь треугольника по 3 сторонам.");
+            Console.WriteLine("3.Выход.");
+        }
+
+        static void CalculateCircleArea()
+        {
+            double result;
+            Console.WriteLine("Введите радиус круга в метрах: ");
+            string userInput = Console.ReadLine();
+
+            if (double.TryParse(userInput, out result))
+            {
+                Circle cirle = new Circle(result);
+                Console.WriteLine($"Площадь круга: {cirle.CalculateArea()} м^2");
+            }
+            else
+            {
+                ShowError();
+            }
+        }
+
+        static void CalculateTriangleArea()
+        {
+            double result;
+            double trianleSideA = 0;
+            double trianleSideB = 0;
+            double trianleSideC = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"Введите длину {i + 1} стороны в см: ");
+                string userInput = Console.ReadLine();
+
+                if (double.TryParse(userInput, out result))
+                {
+                    switch (i)
+                    {
+                        case 0:
+                            trianleSideA = result;
+                            break;
+                        case 1:
+                            trianleSideB = result;
+                            break;
+                        case 2:
+                            trianleSideC = result;
+                            break;
+                    }
+                }
+                else
+                {
+                    ShowError();
+                    return;
+                }
+            }
+
+            Triangle triangle = new Triangle(trianleSideA, trianleSideB, trianleSideC);
+            Console.WriteLine($"Площадь треугольника: {triangle.CalculateArea()} см^2");
+            Console.WriteLine("Является ли треугольник прямоугольным? - " + triangle.IsRectangular());
+        }
+
+        static void ShowError()
+        {
+            Console.WriteLine("Неверный ввод. Попробуйте снова.");
+        }
+    }
+}
---
+++ Geometry/Geometry.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Geometry
+{
+    public class Circle
+    {
+        private double _radius;
+        private double _area;
+
+        public Circle(double radius)
+        {
+            _radius = radius;
+        }
+
+        public double CalculateArea()
+        {
+            _area = (Math.PI * Math.Pow(_radius, 2));
+            return _area;
+        }
+    }
+
+    public class Triangle
+    {
+        private double _sideA;
+        private double _sideB;
+        private double _sideC;
+        private double _area;
+
+        public Triangle(double sideA, double sideB, double sideC)
+        {
+            _sideA = sideA;
+            _sideB = sideB;
+            _sideC = sideC;
+        }
+
+        public double CalculateArea()
+        {
+            double p = (_sideA + _sideB + _sideC) / 2;
+            _area = Math.Sqrt(p * (p - _sideA) * (p - _sideB) * (p - _sideC));
+            return _area;
+        }
+
+        public string IsRectangular()
+        {
+            string rectangular = "Прямоугольный";
+            string notRectangular = "Не прямоугольный";
+
+            if (_sideA > _sideB && _sideA > _sideC)
+            {
+                if (Math.Pow(_sideA, 2) == (Math.Pow(_sideB, 2) + Math.Pow(_sideC, 2)))
+                    return rectangular;
+            }
+            else if (_sideB > _sideA && _sideB > _sideC)
+            {
+                if (Math.Pow(_sideB, 2) == (Math.Pow(_sideA, 2) + Math.Pow(_sideC, 2)))
+                    return rectangular;
+            }
+            else if (_sideC > _sideA && _sideC > _sideB)
+            {
+                if (Math.Pow(_sideC, 2) == (Math.Pow(_sideA, 2) + Math.Pow(_sideB, 2)))
+                    return rectangular;
+            }
+
+            return notRectangular;
+        }
+    }
+}
